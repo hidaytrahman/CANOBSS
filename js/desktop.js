f734e204ea8ce96c1094a48b7cbd9059f40d8c7c
hidaytullah.rahmani <hidaytullah.rahmani@thalesgroup.com>
2019-11-01 11:06:37 +0530

    Creating complete UI with voice recognization

---
+++ js/desktop.js
@@ -0,0 +1,301 @@
+var Desktop = {
+    options: {
+        windowArea: ".window-area",
+        windowAreaClass: "",
+        taskBar: ".task-bar > .tasks",
+        taskBarClass: ""
+    },
+
+    wins: {},
+
+    setup: function (options) {
+        this.options = $.extend({}, this.options, options);
+        return this;
+    },
+
+    addToTaskBar: function (wnd) {
+        var icon = wnd.getIcon();
+        var wID = wnd.win.attr("id");
+        var item = $("<span>").addClass("task-bar-item started").html(icon);
+
+        item.data("wID", wID);
+
+        item.appendTo($(this.options.taskBar));
+    },
+
+    removeFromTaskBar: function (wnd) {
+        var wID = wnd.attr("id");
+        var items = $(".task-bar-item");
+        var that = this;
+        $.each(items, function () {
+            var item = $(this);
+            if (item.data("wID") === wID) {
+                delete that.wins[wID];
+                item.remove();
+            }
+        })
+    },
+
+    createWindow: function (o) {
+        o.onDragStart = function () {
+            win = $(this);
+            $(".window").css("z-index", 1);
+
+            if (!win.hasClass("modal")) {
+                win.css("z-index", 3);
+            }
+        };
+        o.onDragStop = function () {
+            win = $(this);
+            if (!win.hasClass("modal"))
+                win.css("z-index", 2);
+        };
+        o.onWindowDestroy = function (win) {
+            Desktop.removeFromTaskBar($(win));
+        };
+
+        var w = $("<div>").appendTo($(this.options.windowArea));
+        var wnd = w.window(o).data("window");
+
+        var win = wnd.win;
+        var shift = Metro.utils.objectLength(this.wins) * 16;
+
+        if (wnd.options.place === "auto" && wnd.options.top === "auto" && wnd.options.left === "auto") {
+            win.css({
+                top: shift,
+                left: shift
+            });
+        }
+        this.wins[win.attr("id")] = wnd;
+        this.addToTaskBar(wnd);
+
+        return wnd;
+    }
+};
+
+Desktop.setup();
+
+var w_icons = [
+    'rocket', 'apps', 'cog', 'anchor'
+];
+var w_titles = [
+    'rocket', 'apps', 'cog', 'anchor'
+];
+
+function createWindow() {
+    var index = Metro.utils.random(0, 3);
+    var w = Desktop.createWindow({
+        resizeable: true,
+        draggable: true,
+        width: 300,
+        icon: "<span class='mif-" + w_icons[index] + "'></span>",
+        title: w_titles[index],
+        content: "<div class='p-2'>This is desktop demo created with Metro 4 Components Library</div>"
+    });
+
+    setTimeout(function () {
+        w.setContent("New window content");
+    }, 3000);
+}
+
+function createWindowWithCustomButtons() {
+    var index = Metro.utils.random(0, 3);
+    var customButtons = [
+        {
+            html: "<span class='mif-rocket'></span>",
+            cls: "sys-button",
+            onclick: "alert('You press rocket button')"
+        },
+        {
+            html: "<span class='mif-user'></span>",
+            cls: "alert",
+            onclick: "alert('You press user button')"
+        },
+        {
+            html: "<span class='mif-cog'></span>",
+            cls: "warning",
+            onclick: "alert('You press cog button')"
+        }
+    ];
+    Desktop.createWindow({
+        resizeable: true,
+        draggable: true,
+        customButtons: customButtons,
+        width: 360,
+        icon: "<span class='mif-" + w_icons[index] + "'></span>",
+        title: w_titles[index],
+        content: "<div class='p-2'>This is desktop demo created with Metro 4 Components Library.<br><br>This window has a custom buttons in caption.</div>"
+    });
+}
+
+function createWindowModal() {
+    Desktop.createWindow({
+        resizeable: false,
+        draggable: true,
+        width: 300,
+        icon: "<span class='mif-cogs'></span>",
+        title: "Modal window",
+        content: "<div class='p-2'>This is desktop demo created with Metro 4 Components Library</div>",
+        overlay: true,
+        //overlayColor: "transparent",
+        modal: true,
+        place: "center",
+        onShow: function (win) {
+            var win = $(win);
+            win.addClass("ani-swoopInTop");
+            setTimeout(function () {
+                $(win).removeClass("ani-swoopInTop");
+            }, 1000);
+        },
+        onClose: function (win) {
+            var win = $(win);
+            win.addClass("ani-swoopOutTop");
+        }
+    });
+}
+
+function createWindowYoutube() {
+    Desktop.createWindow({
+        resizeable: true,
+        draggable: true,
+        width: 500,
+        icon: "<span class='mif-youtube'></span>",
+        title: "Youtube video",
+        content: "https://youtu.be/ZlhUJ6QkvL8",
+        clsContent: "bg-dark"
+    });
+}
+
+function createSearchBar() {
+    Desktop.createWindow({
+        resizeable: true,
+        draggable: true,
+        width: 500,
+        icon: "<span class='mif-youtube'></span>",
+        title: "Youtube video",
+        content: `
+        <section class="search-area">
+        <div class="search-wrapper">
+            <input type="text" class="input-text" placeholder="Search or speak">
+            <a href="#" class="btn btn-voice"><span class="mif-settings-voice"></span></a>
+        </div>
+    </section>
+    <!-- Search are end here -->`,
+        clsContent: "bg-dark"
+    });
+}
+
+function createGraph() {
+    const fetchUsers = () => {
+        axios.get('http://localhost:8081/ml-ai/data/data.json')
+            .then(response => {
+                const users = response.data;
+                console.log(`GET list data`, users);
+                console.log(`GET list data`, users.length);
+                for(let i=0; i<users.length; i++) {
+                    console.log(users[i]);
+                    let _user = users[i];
+                    document.querySelector('#results-data #tdata').innerHTML += '<tr><td>'+_user.IP_ADDRESS+'</td></tr>';
+                }
+                  
+            })
+            .catch(error => console.error(error));
+    };
+    
+    fetchUsers();
+
+    Desktop.createWindow({
+        resizeable: true,
+        draggable: true,
+        width: 700,
+        icon: "<span class='mif-flow-parallel'></span>",
+        title: "Data Graph",
+        content: `<h2>Hidayt</h2> <hr>
+        <table id="results-data">
+            <thead>
+            <tr>
+                <th>IP Address</th>
+                <th>Contact</th>
+                <th>Country</th>
+            </tr>
+            </thead>
+            <tbody id="tdata">
+            </tbody>
+        </table>
+        `,
+        clsContent: "bg-light"
+    });
+}
+
+
+function createNodeDiff() {
+    Desktop.createWindow({
+        resizeable: true,
+        draggable: true,
+        width: 700,
+        icon: "<span class='mif-done_all'></span>",
+        title: "Node comparison",
+        content: `
+        <div class="diff-section">
+        <h2 class="model-title">Node Diff. between 2 Dates</h2> <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+        </div>
+        `,
+        clsContent: "bg-light"
+    });
+}
+
+
+
+function openCharm() {
+    var charm = $("#charm").data("charms");
+    charm.toggle();
+}
+
+$(".window-area").on("click", function () {
+    Metro.charms.close("#charm");
+});
+
+$(".charm-tile").on("click", function () {
+    $(this).toggleClass("active");
+});
+
+
+function setAlert() {
+
+    var inputs = $(".set-alert .input-area");
+
+    var notificationsList = [];
+
+    $(".submit-button").on("click", function () { 
+        var something1 = inputs.find('.something1').val();
+        var something2 = inputs.find('.something2').val();
+        var something3 = inputs.find('.something3').val();
+        
+        var notification = something1+' of a '+something2+' by '+something3;
+        notificationsList.push(notification)
+        localStorage.setItem('notificationList',notificationsList)
+    });
+}
+
+function getAlerts() {
+   var notificationExist =  localStorage.getItem('notificationList');
+   if(notificationExist) {
+        var notificationsList = notificationExist.split(',');
+        console.log('notificationsList',notificationsList);
+        for(var  i = 0; i<notificationsList.length; i++) {
+            $(".alerts-listing").append('<p>'+notificationsList[i]+'</p>');
+        }
+        
+   }
+  
+}
+
+setAlert();
+getAlerts();
+
+/* Just to experiments 
+(() => {
+    createSearchBar();
+})()
+*/
