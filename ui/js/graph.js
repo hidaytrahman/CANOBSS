1ffa437310a7be67c4c9b14c63451253626a20f9
heartofrevel <anshul.edu@live.com>
2019-11-07 15:43:23 +0530

    Ajax Call FIX to check file exist

--- ui/js/graph.js
+++ ui/js/graph.js
@@ -14,7 +14,7 @@ function draw(date, id) {
 
     var nodes_list = [];
 	var svg;
-   if(id = "mainSvg"){
+   if(id == "mainSvg"){
 		 svg = d3.select("#" + id)
 			.attr("width", width)
 		.call(responsivefy);
@@ -460,8 +460,8 @@ function createJSONTimeGraph() {
     var file2Exists = false;
     var filePath1 = '/json/graph-'+finalDate1+'.json';
     var filePath2 = '/json/graph-'+finalDate2+'.json';
-    file1Exists = checkFileExists(filePath1);
-    file2Exists = checkFileExists(filePath2);
+    file1Exists = fileExists(filePath1);
+    file2Exists = fileExists(filePath2);
 
     if(!file1Exists){
         $.ajax({
@@ -514,9 +514,6 @@ function createTimeGraphs(){
     createJSONTimeGraph();
     draw(dateValues[2], "timeSvg1");
     draw(dateValues[0], "timeSvg2");
-
-    getTop5Diff(dateValues[2], dateValues[0]);
-  
 }
 
 function getTop5Diff(date1, date2){
@@ -624,15 +621,11 @@ function popupClose2()
 	ele1.innerHTML="";
 }
 
-function checkFileExists(filePath){
-    $.ajax({
-        type: 'HEAD',
-        url: filePath,
-        success: function(msg){
-          return true;
-        },
-        error: function(jqXHR, textStatus, errorThrown){
-            return false;
-        }
-      });
+
+function fileExists(url)
+{
+    var http = new XMLHttpRequest();
+    http.open('HEAD', url, false);
+    http.send();
+    return http.status==200;
 }
